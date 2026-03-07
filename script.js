70831beb3dd5ce2ed12c001b3ad2d65383f23392
samuelepotalivo56-sys <samuelepotalivo56@gmail.com>
2026-03-07 09:52:44 +0000

    counter

---
+++ script.js
@@ -0,0 +1,29 @@
+let prima_variabile = 0
+let mio_pulsante = document.querySelector("#id")
+
+let aggiungi = document.querySelector("#aggiungi");
+let sottrai = document.querySelector("#sottrai");
+
+console.log(aggiungi, sottrai);
+
+aggiungi.addEventListener("click", function () {
+ let output = document.querySelector("#output");
+ let result = Number(output.innerText) + 1;
+  if (result > 10) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+
+sottrai.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+let result = Number(output.innerText) + 1;
